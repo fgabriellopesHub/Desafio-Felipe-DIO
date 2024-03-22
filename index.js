bb29034e4a8d6d88e8bfff74be5866ad19362373
fgabriellopesHub <contatogabriellopesdev@gmail.com>
2024-03-22 12:05:03 -0300

    Teste projeto 1

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-let testeGitHub = "Primeiro teste"
-console.log(testeGitHub)
+let nomeDoHeroi = "Bigodinho"
+let niveisDoHeroi = [1000, 1001, 2000,2001, 5000, 5001, 7000, 7001, 8000, 8001, 9000, 9001, 10000, 10001]
+let ranking = [
+    "Ferro",
+    "Bronze",
+    "Ouro",
+    "Platina",
+    "Ascendente",
+    "Imortal"
+]
+
+switch(1000,10001){
+    case 1000:
+        console.log("O herói de nome " + nomeDoHeroi + " está no ranking " + ranking[0])
+        break
+        case 1001>=2000:
+            console.log("O herói de nome " + nomeDoHeroi + " está no ranking " + ranking[1])
+       
+}
