82e2d2fe8155aa15b7699fd5ae8c90305b454f6c
fgabriellopesHub <contatogabriellopesdev@gmail.com>
2024-03-25 19:16:24 -0300

    Atualizando projeto

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let nomeDoHeroi = "Astro"
 let nivelDoHeroi = 10001
-let experienciaDoHeroi = [1000, 1001, 2000, 2001, 5000, 5001, 7000, 7001, 8000, 8001, 9000, 9001, 10000, 10001]
-let ranking = [
+let experienciaDoHeroi = 1000
+ 2000, 2001, 5000, 5001, 7000, 7001, 8000, 8001, 9000, 9001, 10000, 10001
+let ranking=[
     "Ferro",
     "Bronze",
     "Prata",
@@ -11,8 +12,29 @@ let ranking = [
     "Imortal",
     "Radiante",
 ]
-do {
-console.log(nivelDoHeroi -= 10002)
-console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + ranking[7]+ "!")
+switch (nivelDoHeroi){
+    case 1000:
+     console.log("O Herói de nome "+ nomeDoHeroi+ " está no nivél de " + ranking[0]+"!")
+     break
+    case 1001,2000:
+      console.log("O Herói de nome "+ nomeDoHeroi+ " está no nivél de " + ranking[1]+"!")
+    break
+    case 2001,5000:
+      console.log("O Herói de nome "+ nomeDoHeroi+ " está no nivél de " + ranking[2]+"!")
+    break
+    case 5001,7000:
+      console.log("O Herói de nome "+ nomeDoHeroi+ " está no nivél de " + ranking[3]+"!")
+    break
+    case 7001,8000:
+      console.log("O Herói de nome "+ nomeDoHeroi+ " está no nivél de " + ranking[4]+"!")
+    break
+    case 8001,9000:
+      console.log("O Herói de nome "+ nomeDoHeroi+ " está no nivél de " + ranking[5]+"!")
+    break
+    case 9001,10000:
+      console.log("O Herói de nome "+ nomeDoHeroi+ " está no nivél de " + ranking[6]+"!")
+    break
+    case 10001:
+      console.log("O Herói de nome "+ nomeDoHeroi+ " está no nivél de " + ranking[7]+"!")
+    break
 }
-while(experienciaDoHeroi >= 10002)
